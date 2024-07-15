876234da77b0c2ee71378f8685946f828b0a5cf0
eduardo caballero <156390561+imeddyhi@users.noreply.github.com>
2024-07-15 13:57:32 -0600

    Update search.php

--- search.php
+++ search.php
@@ -62,36 +62,54 @@
     </style>
 </head>
 <body>
-    <?php
-    $bd_host = "127.0.0.1";
-    $bd_user = "root";
-    $bd_pass = "";
-    $bd_name = "fruteria";
-    $precio_min = htmlspecialchars($_POST["precio_min"]);
-    $precio_max = htmlspecialchars($_POST["precio_max"]);
+        <main>
+            <div class="canvas">
+                <?php
+                    $bd_host = "127.0.0.1";
+                    $bd_user = "root";
+                    $bd_pass = "";
+                    $bd_name = "fruteria";
+                    $precio_min = htmlspecialchars($_POST["precio_min"]);
+                    $precio_max = htmlspecialchars($_POST["precio_max"]);
+                    
+                    # mysqli_conmect - Abre una nueva conexión al servidor de MySQÑ
+                    $conectar = mysqli_connect($bd_host, $bd_user, $bd_pass, $bd_name, 3306);
 
-    $conectar = mysqli_connect($bd_host, $bd_user, $bd_pass, $bd_name, 3306);
-    if (mysqli_connect_errno()) {
-        printf("ERROR: %u - %s", mysqli_connect_errno(), mysqli_connect_error());
-        exit();
-    }
+                    # mysqli_connect_errno - Devuleve el codigo de error de la ultima llamda
+                    if (mysqli_connect_errno())
+                    {
+                        # mysqli_connect_error - Devulve un cade con la descripción del ultimo error de conexión
+                        printf("ERROR: %u - %s", mysqli_connect_errno(), mysqli_connect_error());
+                        exit();
+                    }
 
-    mysqli_set_charset($conectar, "utf8");
-    $consultar = "SELECT * FROM frutas WHERE precio_fruta";
-    if ($resultado = mysqli_query($conectar, $consultar)) {
-        printf("<table><tr><th>Código</th><th>Nombre</th><th>Variedad</th><th>Clase</th><th>Precio</th><th>Cantidad</th></tr>");
-        while ($fila = mysqli_fetch_row($resultado)) {
-            printf("<tr><td>%d</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $fila[0], $fila[1], $fila[2], $fila[3], $fila[4], $fila[5]);
-        }
-        printf("</table>");
-    } else {
-        echo 'No se encontraron registros dentro del rango de precios.';
-    }
-        mysqli_free_result($resultado);
-    else {
-        echo 'Error en la consulta: '  mysqli_error($conectar);
-    }
-    mysqli_close($conectar);
-    ?>
-</body>
+                    # mysqli_set_charset - Establece el conjunto de caracteres predeterminados del cliente
+                    mysqli_set_charset($conectar, "utf8");
+                    $consultar = "SELECT * FROM frutas WHERE precio_fruta BETWEEN $precio_min AND $precio_max";
+
+                    # mysqli_query - Realiza una consulta a la base de datos
+                    if ($resultado = mysqli_query($conectar, $consultar))
+                    {
+                        printf("<table><tr><th>C&oacute;digo</th> <th>Nombre</th> <th>Variedad</th> <th>Clase</th> <th>Precio</th> <th>Cantidad</th></tr>");
+
+                        # mysqli_fetch_row - Obtener una fila de resultados como array enumerado
+                        while ($fila = mysqli_fetch_row($resultado))
+                        {
+                            printf("<tr><td>%d</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $fila[0], $fila[1], $fila[2], $fila[3], $fila[4], $fila[5]);
+                        }
+                        printf("</table>");
+
+                        #mysqli_free_result - Libera la memoria asociada a un resultado
+                        mysqli_free_result($resultado);
+                    }
+
+                    #mysqli_close - Cierra una conexión previamente abierta  una base de datos
+                    mysqli_close($conectar);
+                ?>
+            </div>
+        </main>
+        <div class="de1">
+            <a href="interfaz_ingreso.html" class="hhh">Registrar nueva fruta</a>
+        </div>
+    </body>
 </html>
